8b87045cd9dc3c0e202d2d15b87fad0af60c1ab4
RealElvince <obuongely@yahoo.com>
2025-08-02 17:06:07 +0300

    transfers from gcs to bigquery wht

--- dags/elt_dags.py
+++ dags/elt_dags.py
@@ -3,7 +3,8 @@ from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmpt
 from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmptyDatasetOperator
 from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
 from airflow.providers.google.cloud.operators.gcs import GCSCreateBucketOperator
-from airflow.providers.google.cloud.operators.bigquery import GCSToBigQueryOperator
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
+
 
 from datetime import timedelta, datetime
 import os
